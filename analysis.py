5d42ce0f054490cfc0dbe086c53fac6fc3a8b8de
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-03-07 18:16:48 +0530

    some changes to gitig + analysis

--- analysis.py
+++ analysis.py
@@ -1,17 +1,24 @@
-from model.extract import get_date, get_invoice_no
+from model.extract import get_date, get_invoice_no, get_address
 import os
 
-for fname in os.listdir('text')[:1]:
+for fname in (os.listdir('text'))[:14]:
+    print("=========Analysis for file {} ==============".format(fname))
     with open('text/' + fname, 'r') as f:
         data = f.readlines()
-    folder_name = 'parse_text' + os.sep + fname
+    folder_name = './parsed_text' + os.sep + fname
     # os.system('mkdir ' + folder_name)
     
-    # with open(folder_name+'/date.txt', 'w') as f:
+    with open(folder_name+'/date.txt', 'w') as f:
         # print(list(get_date(data)))
-        # f.writelines(list(get_date(data)))
+        f.writelines(list(get_date(data)))
 
     with open(folder_name+'/invoice_no.txt', 'w') as f:
         print(list(get_invoice_no(data)))
-        # f.writelines(list(get_date(data)))
-        # get_invoice_no(data)
+        f.writelines(list(get_invoice_no(data)))
+    
+    with open(folder_name+'/addre\n', '(Shop #:1ss.txt', 'w') as f:
+        print(get_address(data))
+        # f.writelines(list(get_invoice_no(data)))
+    with open(folder_name+'/store_name.txt', 'w') as f:
+        # print(list(get_store_name(data)))
+        f.writelines(list(str(get_store_name(data))))
--- model/extract.py
+++ model/extract.py
@@ -1,18 +1,16 @@
 import operator 
 import sys
 import re
-import nltk
-# from nltk.corpus import stopwords
 import os
 # import pandas as pd 
-
+import json
 
 def get_date(items):
 	#format d/m/y & d-m-y is support
     dates=set()
     # print(items)
     for data in items:
-        f1 = re.findall(r'\d{1,2}\(/,.,-)\d{1,2}\/\d{4}',data)
+        f1 = re.findall(r'\d{1,2}\/\d{1,2}\/\d{4}',data)
         f2 = re.findall(r'\d{4}\/\d{1,2}\/\d{1,2}',data)
         f3 = re.findall(r'\d{2}\/\d{1,2}\/\d{1,2}',data)
         f4 = re.findall(r'\d{1,2}\/\d{1,2}\/\d{2}',data)
@@ -28,17 +26,24 @@ def get_date(items):
     return dates
 
 def get_invoice_no(data):
-    print(data)
-    invoice_no = set()
-    for i in data:
-        if 'invoice' in i.lower().strip().split():
-            print(i)
-        if 'bill' in i.lower().strip().split():
-            print(i)
-            for words in i.lower().strip().split():
-                try:
-                    x = int(words) + 0
-                    invoice_no.add(x)
-                except:
-                    continue
-    return invoice_no
+    return ''
+
+def get_address(data):
+    import requests
+    x = requests.get('https://indian-cities-api-nocbegfhqg.now.sh/cities')
+    cities = json.loads(x.text)
+    idx = -1
+    for j in cities:
+        for i in data[:10]:
+            comp = i.lower().strip().strip('\n').split()
+            if j['City'].lower() in comp:
+                idx = data.index(i)
+                break
+    print(data[max(0,idx-2):idx+1])
+    return data[max(0,idx-2):idx+1]
+
+def get_store_name(data):
+    store_name = set()
+    existing_stores = os.listdir('Training Data Set')
+    existing_stores.append('PHOENIX MALL')
+    
