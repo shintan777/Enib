b6185fda874ba4c113d47371b9e580ddfd1e71ff
shintan777 <tanvi.shinde46@gmail.com>
2020-03-08 07:02:04 +0530

    merging modules

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import sys
 from model.ocr import ocr
-from model.extact  import *
+from model.extract  import *
 
 def run(imgpath):
     text, lines = ocr(imgpath)
+    json = {}
+
+    date = get_date(lines)
+    json['date'] = date['date']
+
+    time = get_time(lines)
+    json['time'] = time
     
+    invoice_no = get_invoice_no(lines)
+    json['invoice_no'] = invoice_no
+
+    address = get_address(lines)
+    json['address'] = address
+
+    store_name = get_store_name(lines)
+    json['store_name'] = store_name
+
+    total_amount = get_total_amount(lines)
+    json['total_amount'] = total_amount
+
+    print(json)
+
+
+
+
+
+
 
 
 
